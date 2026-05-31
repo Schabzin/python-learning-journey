80d3998412234e2071fa8c062d5a68bcb0024625
Sechaba <schabzin18@gmail.com>
2026-05-31 18:05:12 +0200

    Day 48 Session 2 - Blueprint error handling

--- day48a.py
+++ day48a.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(dashboard, url_prefix="/dashboard")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Page not found, Separaka Taxi System", 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return "Server error, contact admin", 500
+
 if __name__ == "__main__":
     app.run(debug=True)
