b6d713389cf864583ff12ca37f6758494fe4b9e0
Sechaba <schabzin18@gmail.com>
2026-08-09 16:52:36 +0200

    Day 84 - versioned service worker cache, fixes stale JavaScript being served after deploys

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,19 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/static/sw.js")
-        .then(function () { console.log("Service worker registered"); })
-        .catch(function (error) { console.error("Service worker registration failed:", error); });
-}
+const CACHE_VERSION = "v2";
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (name) {
+                    if (name !== CACHE_VERSION) {
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(fetch(event.request));
+});
